67ae9940e18a4ae52c89869fd841ad9c9f9ff07d
Ronald patiño <patinoguevararonald@gmail.com>
2024-07-27 17:12:51 -0500

    elaboracion menu terminada

--- src/Solve.java
+++ src/Solve.java
@@ -2,20 +2,57 @@ import java.util.Scanner;
 
 public class Solve {
     public static void main(String[] args) {
-       Scanner scanner= new Scanner(System.in);
+        inicio();
+    }
 
-       do {
-           String opcion="Elija una opcion\n\n";
-           opcion+= "1. Motrar libros\n";
-           opcion+= "2. Eliminar libro\n";
-           opcion+= "3. Agregar Libro\n";
-           opcion+= "4. Listar libros\n";
-           opcion+= "5. Resumen de segmento\n";
-           opcion+= "6. Funciones de segmento\n";
-           System.out.println(opcion);
-           switch (opcion){
+    public static void inicio(){
+        Scanner scanner = new Scanner(System.in);
+        String menu, menu2;
+        int opcion,opcion2, continuar;
+        do {
+            menu="Elija una opcion\n\n";
+            menu+= "1. Casteo\n";
+            menu+= "2. Operadores de Asignacion\n";
+            menu+= "3. Operadores de incremento y decremento\n";
+            menu+= "4. Ejercicios combinados\n";
+            System.out.println(menu);
+            opcion=scanner.nextInt();
+            switch (opcion){
+                case 1:
+                    menu2="Elija una opcion\n\n";
+                    menu2+= "1. Ejercicio 1\n";
+                    menu2+= "2. Ejercicio 2\n";
+                    menu2+= "3. Ejercicio 3\n";
+                    menu2+= "4. Ejercicio 4\n";
+                    System.out.println(menu2);
+                    opcion2=scanner.nextInt();
+                    switch (opcion2){
+                        case 1:
+                            System.out.println(Main.ejercicio1());
+                            break;
+                        case 2:
+                            System.out.println(Main.ejercicio2());
+                            break;
+                        case 3:
+                            System.out.println(Main.ejercicio3());
+                            break;
+                        case 4:
+                            System.out.println(Main.ejercicio4());
+                            break;
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:System.out.println("Opcion invalida");
 
-           }
+            }
+            System.out.print("Desea continuar 1=Si/2=No ");
+            continuar= scanner.nextInt();
 
-       } while ();
-}}
+        } while (continuar==1);
+    }
+}
