f3b0a4c37a33da5536040424c8eb927c1b229372
Ronald patiño <patinoguevararonald@gmail.com>
2024-07-27 19:11:46 -0500

    ejercicios 15 listos

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println(ejercicio1());
         System.out.println(ejercicio2());
         System.out.println(ejercicio3());
-        System.out.println(ejercicio4());
+        System.out.println(ejercicio15());
     }
 
         public static int ejercicio1() {
@@ -32,6 +32,78 @@ public class Main {
             int myInt2=Integer.parseInt(mystring);
             return myInt2;
         }
+
+        public static int ejercicio5() {
+            int resultado = (int) (char) (byte) -1;
+            return resultado;
+        }
+
+        public static char ejercicio6() {
+            int myint = 65;
+            char mychar = (char) myint;
+            return  mychar;
+        }
+
+        public static int ejercicio7() {
+            long myLong = 978;
+            int myInt = (int) myLong;
+            return  myInt;
+        }
+
+        public static String ejercicio8() {
+            float myFloat = 3.45f;
+            String mystring = Float.toString(myFloat);
+            return  mystring;
+        }
+
+        public static int ejercicio9() {
+            int resultado = (int) Math.round(3.7);
+            return resultado;
+        }
+
+        public static int ejercicio10() {
+            byte mybyte = 50;
+            short myshort = (byte) mybyte;
+            return myshort;
+        }
+
+        //Operadores de Asignacion
+
+        public static int ejercicio11() {
+            int x = 10;
+            x += 5;
+            return x;
+        }
+
+        public static int ejercicio12() {
+            int y = 10;
+            int x=3;
+            y *= x+5;
+            return y;
+        }
+
+        public static int ejercicio13() {
+            int a = 15;
+            a %= 4;
+            return a;
+        }
+
+        public static int ejercicio14() {
+            int x = 15;
+            int y = 5;
+            x += y;
+            return x;
+        }
+
+        public static int ejercicio15() {
+            int x = 8;
+            x ^= 2;
+            return x;
+        }
+
+
+
+
 
 
 
--- src/Solve.java
+++ src/Solve.java
@@ -7,8 +7,8 @@ public class Solve {
 
     public static void inicio(){
         Scanner scanner = new Scanner(System.in);
-        String menu, menu2;
-        int opcion,opcion2, continuar;
+        String menu, menu2, menu3;
+        int opcion,opcion2,opcion3, continuar;
         do {
             menu="Elija una opcion\n\n";
             menu+= "1. Casteo\n";
@@ -24,6 +24,12 @@ public class Solve {
                     menu2+= "2. Ejercicio 2\n";
                     menu2+= "3. Ejercicio 3\n";
                     menu2+= "4. Ejercicio 4\n";
+                    menu2+= "5. Ejercicio 5\n";
+                    menu2+= "6. Ejercicio 6\n";
+                    menu2+= "7. Ejercicio 7\n";
+                    menu2+= "8. Ejercicio 8\n";
+                    menu2+= "9. Ejercicio 9\n";
+                    menu2+= "10. Ejercicio 10\n";
                     System.out.println(menu2);
                     opcion2=scanner.nextInt();
                     switch (opcion2){
@@ -39,15 +45,63 @@ public class Solve {
                         case 4:
                             System.out.println(Main.ejercicio4());
                             break;
+                        case 5:
+                            System.out.println(Main.ejercicio5());
+                            break;
+                        case 6:
+                            System.out.println(Main.ejercicio6());
+                            break;
+                        case 7:
+                            System.out.println(Main.ejercicio7());
+                            break;
+                        case 8:
+                            System.out.println(Main.ejercicio8());
+                            break;
+                        case 9:
+                            System.out.println(Main.ejercicio9());
+                            break;
+                        case 10:
+                            System.out.println(Main.ejercicio10());
+                            break;
+                        default:System.out.println("Opcion invalida");
+                            break;
                     }
                     break;
                 case 2:
+                    menu3="Elija una opcion\n\n";
+                    menu3+= "1. Ejercicio 1\n";
+                    menu3+= "2. Ejercicio 2\n";
+                    menu3+= "3. Ejercicio 3\n";
+                    menu3+= "4. Ejercicio 4\n";
+                    menu3+= "5. Ejercicio 5\n";
+                    System.out.println(menu3);
+                    opcion3=scanner.nextInt();
+                    switch (opcion3){
+                        case 1:
+                            System.out.println(Main.ejercicio11());
+                            break;
+                        case 2:
+                            System.out.println(Main.ejercicio12());
+                            break;
+                        case 3:
+                            System.out.println(Main.ejercicio13());
+                            break;
+                        case 4:
+                            System.out.println(Main.ejercicio14());
+                            break;
+                        case 5:
+                            System.out.println(Main.ejercicio15());
+                            break;
+                        default:System.out.println("Opcion invalida");
+                            break;
+                    }
                     break;
                 case 3:
                     break;
                 case 4:
                     break;
                 default:System.out.println("Opcion invalida");
+                break;
 
             }
             System.out.print("Desea continuar 1=Si/2=No ");
